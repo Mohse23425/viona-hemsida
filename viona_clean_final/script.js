3760436e5248f3d43cc97203091ed88c23aac91e
Mohse23425 <viona377xx@gmail.com>
2025-11-14 23:23:34 +0100

    Delete viona_clean_final directory

--- viona_clean_final/script.js
+++
@@ -1,8 +0,0 @@
-console.log('clean loaded');
-document.querySelectorAll('.glow-follow').forEach(btn=>{
- btn.addEventListener('mousemove',e=>{
-  const r=btn.getBoundingClientRect();
-  btn.style.setProperty('--x',`${e.clientX-r.left}px`);
-  btn.style.setProperty('--y',`${e.clientY-r.top}px`);
- });
-});
