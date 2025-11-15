5a48fb61e945486c2c0a2bd362c3ead6d9f13f24
Mohse23425 <viona377xx@gmail.com>
2025-11-15 17:02:18 +0100

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-console.log('clean loaded');
-document.querySelectorAll('.glow-follow').forEach(btn=>{
- btn.addEventListener('mousemove',e=>{
-  const r=btn.getBoundingClientRect();
-  btn.style.setProperty('--x',`${e.clientX-r.left}px`);
-  btn.style.setProperty('--y',`${e.clientY-r.top}px`);
- });
+console.log('viona glow active');
+
+document.querySelectorAll('.glow-follow').forEach(btn => {
+  btn.addEventListener('mousemove', e => {
+    const r = btn.getBoundingClientRect();
+    btn.style.setProperty('--x', `${e.clientX - r.left}px`);
+    btn.style.setProperty('--y', `${e.clientY - r.top}px`);
+  });
 });
